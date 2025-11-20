9a9888a700c0f125ebab989c87c1ac6189b39c4c
Gabriel <lovanomeny72@gmail.com>
2025-11-20 07:23:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+﻿const http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from DevOps demo (Windows)!');
+}).listen(port, () => console.log('Server running on port ' + port));
